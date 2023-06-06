47211b9272c57abf1f08ddf32efe14cf351363a4
maurauca <maura.quintanilla15903@est.uca.edu.ni>
2023-06-06 16:55:55 -0600

    Se resuelve error de bucle en add participante

--- codigo/participante.cpp
+++ codigo/participante.cpp
@@ -10,54 +10,42 @@ typedef struct
 {
     char facultad[MAX];
     char nombre[40];
-    int año;
+    char año[9];
 } carrera;
 
 typedef struct
 {
     char cod[10];
-    char nombre[25];
-    char apellido[25];
-    char edad[2];
+    char nombre[35];
+    char apellido[30];
+    char edad[7];
     char contacto[8];
     char departamento[MAX];
     carrera car;
 } participante;
 
-carrera carre[MAX];
 participante parti[MAX];
 int UltReg = 0;
 
-char idCarrera[9];
-carrera carreraSel;
-int posCarrera;
 
 // FUNCIONES CRUD
 
 // CREATE
 void addStudent(participante par);
-void addCareer(carrera car);
-
 // READ
 void showStudent(int pos);
-void showCareer(int pos);
 void showStudents();
-void showCareers();
 
 int isStudent(char cod[]);
 
 void startStudent(int pos);
-void startCareer(int pos);
 participante getStudent(int pos);
-carrera getCarrera(int pos);
 
 // UPDATE
 void updateStudent(participante par, int pos);
-void updateCareer(carrera car, int pos);
 
 // DELETE
 void deleteStudent(int pos);
-void deleteCareer(int pos);
 
 // FICHEROS
 FILE *registroParticipante;
@@ -99,37 +87,33 @@ void addStudent(participante par)
     parti[UltReg] = par;
     UltReg++;
 }
-void addCareer(carrera car)
-{
-    carre[UltReg] = car;
-    UltReg++;
-}
+
 
 void showStudent(int pos)
 {
-    gotoxy(10,2);
+    gotoxy(10, 2);
     cout << "------------ Datos estudiante ------------" << endl;
-    gotoxy(10,3);
+    gotoxy(10, 3);
     cout << "Codigo: " << parti[pos].cod << endl;
-    gotoxy(10,4);
+    gotoxy(10, 4);
     cout << "Nombres: " << parti[pos].nombre << endl;
-    gotoxy(10,5);
+    gotoxy(10, 5);
     cout << "Apellidos: " << parti[pos].apellido << endl;
-    gotoxy(10,6);
+    gotoxy(10, 6);
     cout << "Edad: " << parti[pos].edad << endl;
-    gotoxy(10,7);
+    gotoxy(10, 7);
     cout << "Numero de contacto: " << parti[pos].contacto << endl;
-    gotoxy(10,8);
+    gotoxy(10, 8);
     cout << "Departamento: " << parti[pos].departamento << endl;
+    gotoxy(10,9);
+    cout << "------------ Datos carrera ------------" << endl;
+    gotoxy(10,10);
+    cout << "Facultad: " << parti[pos].car.facultad << endl;
+    gotoxy(10,11);
     cout << "Carrera: " << parti[pos].car.nombre << endl;
+    gotoxy(10,12);
+    cout << "Ano cursante: " << parti[pos].car.año << endl;
 
-}
-
-void showCareer(int pos)
-{
-    cout << "Año que cursa: " << carre[pos].año << endl;
-    cout << "Facultad: " << carre[pos].facultad << endl;
-    cout << "Carrera: " << carre[pos].nombre << endl;
 }
 
 int isStudent(char cod[9])
@@ -146,27 +130,16 @@ int isStudent(char cod[9])
     return pos;
 }
 
-
 participante getStudent(int pos)
 {
     return parti[pos];
 }
-
-carrera getCarrera(int pos)
-{
-    return carre[pos];
-}
-
 
 void updateStudent(participante par, int pos)
 {
     parti[pos] = par;
 }
 
-void updateCareer(carrera car, int pos)
-{
-    carre[pos] = car;
-}
 
 void deleteStudent(int pos)
 {
@@ -183,50 +156,20 @@ void deleteStudent(int pos)
     startStudent(UltReg);
 }
 
-void deleteCareer(int pos)
-{
-    if (pos == UltReg)
-    {
-        cout << "No tenemos un registro de estos datos." << endl;
-        for (int i = 0; i < UltReg; i++)
-        {
-            carre[i] = carre[i + 1];
-        }
-        UltReg--;
-        startCareer(UltReg);
-    }
-}
 
 void showStudents()
 {
     system("cls");
     if (UltReg == 0)
     {
-        cout << "No existe un registro de este participante." << endl;
+        cout << "No existe registro de participantes" << endl;
         return;
     }
     for (int i = 0; i < UltReg; i++)
     {
-        cout << "+++++++++++++++++++++++++++++++++++" << endl;
         showStudent(i);
     }
     cout << "Ultimo registro de estudiante... " << endl;
-}
-
-void showCareers()
-{
-    system("cls");
-    if (UltReg == 0)
-    {
-        cout << "No existe un registro de esta carrera en el club." << endl;
-        return;
-    }
-    for (int i = 0; i < UltReg; i++)
-    {
-        cout << "+++++++++++++++++++++++++++++++++++" << endl;
-        showCareer(i);
-    }
-    cout << "Ultimo registro de carrera... " << endl;
 }
 
 void startStudent(int pos)
@@ -236,15 +179,9 @@ void startStudent(int pos)
     strcpy(parti[pos].apellido, "");
     strcpy(parti[pos].edad, "");
     strcpy(parti[pos].contacto, "");
-    strcpy(parti[pos].departamento, "");
-}
-
-void startCareer(int pos)
-{
-    strcpy(carre[pos].facultad, "");
-    strcpy(carre[pos].nombre, "");
-    carre[pos].año = 0;
-
+    strcpy(parti[pos].car.facultad, "");
+    strcpy(parti[pos].car.nombre, "");
+    strcpy(parti[pos].car.año, "");
 }
 
 int menu()
@@ -255,14 +192,13 @@ int menu()
     cout << "'                   UCA                 '" << endl;
     cout << "'         CLUB DE GUITARRA 2023         '" << endl;
     cout << "'---------------------------------------'" << endl;
-    cout << "Cantidad de estudiantes inscritos: " << UltReg <<endl;
+    cout << "Cantidad de estudiantes inscritos: " << UltReg << endl;
     cout << "1. Inscribir nuevo participante" << endl;
     cout << "2. Editar registro de participante" << endl;
     cout << "3. Eliminar participante" << endl;
     cout << "4. Buscar datos de inscripcion de participante" << endl;
     cout << "5. Mostrar todas los participantes" << endl;
     cout << "6. Salir del programa." << endl;
-    cout << "7. Agregar carrera." << endl;
     cin >> op;
     return op;
 }
@@ -281,6 +217,8 @@ void start()
         {
         case 1:
             system("cls || clear");
+            gotoxy(10,4);
+            cout << "---------------DATOS PERSONALES---------------" << endl;
             gotoxy(10, 5);
             cout << "Id del estudiante:";
             gotoxy(10, 6);
@@ -293,13 +231,13 @@ void start()
             cout << "Ingrese el numero de contacto que proporciono el estudiante: ";
             gotoxy(10, 10);
             cout << "Ingrese el departamento de residencia del estudiante: ";
-            gotoxy(10, 11);
-            cout << "Escribe el codigo de la carrera: " << endl;
+            gotoxy(10,11);
+            cout << "---------------DATOS DE CARRERA---------------" << endl;
             gotoxy(10, 12);
             cout << "Ingrese la facultad de la Universidad: " << endl;
-            gotoxy(10,13);
+            gotoxy(10, 13);
             cout << "Ingrese el nombre de la carrera: " << endl;
-            gotoxy(10,14);
+            gotoxy(10, 14);
             cout << "Ingrese el ano que esta cursando este participante: " << endl;
             gotoxy(29, 5);
             scanf(" %[^\n]", par.cod);
@@ -313,14 +251,12 @@ void start()
             scanf(" %[^\n]", par.contacto);
             gotoxy(64, 10);
             scanf(" %[^\n]", par.departamento);
-            gotoxy(43,11);
-            scanf(" %[^\n]", idCarrera);
-            gotoxy(49,12);
+            gotoxy(49, 12);
             scanf(" %[^\n]", par.car.facultad);
-            gotoxy(43,13);
+            gotoxy(43, 13);
             scanf(" %[^\n]", par.car.nombre);
-            gotoxy(62,14);
-            scanf(" %[^\n]", par.car.año);
+            gotoxy(62, 14);
+            cin >> par.car.año;
             addStudent(par);
             cout << "Participante anadido con exito." << endl;
             system("pause");
@@ -328,39 +264,37 @@ void start()
 
         case 2:
             system("cls||clear");
-            gotoxy(10,1);
+            gotoxy(10, 1);
             cout << "Escribe el codigo del participante a buscar: " << endl;
-            gotoxy(55,1);
+            gotoxy(55, 1);
             scanf(" %[^\n]", cod);
             pos = isStudent(cod);
             showStudent(pos);
-            gotoxy(10,10);
+            gotoxy(2, 10);
             cout << "------------ Datos a editar ------------" << endl;
             gotoxy(10, 11);
             cout << "Id del estudiante:";
             gotoxy(10, 12);
-            cout << "Ingrese el nombre del estudiante: ";
+            cout << "Ingrese los nombres del estudiante: ";
             gotoxy(10, 13);
-            cout << "Ingrese el apellido del estudiante: ";
+            cout << "Ingrese los apellidos del estudiante: ";
             gotoxy(10, 14);
             cout << "Ingrese la edad del estudiante: ";
             gotoxy(10, 15);
             cout << "Ingrese el numero de contacto que proporciono el estudiante: ";
             gotoxy(10, 16);
             cout << "Ingrese el departamento de residencia del estudiante: ";
-            gotoxy(10, 17);
-            cout << "Ingrese el codigo de la carrera: " << endl; 
             gotoxy(10, 18);
-            cout << "Ingrese la facultad: " << endl; 
+            cout << "Ingrese la facultad de la Universidad: " << endl;
             gotoxy(10, 19);
-            cout << "Ingrese el nombre de la carrera: " << endl; 
+            cout << "Ingrese el nombre de la carrera: " << endl;
             gotoxy(10, 20);
-            cout << "Ingrese el año cursante: " << endl; 
+            cout << "Ingrese el ano que esta cursando este participante: " << endl;
             gotoxy(29, 11);
             scanf(" %[^\n]", par.cod);
-            gotoxy(44, 12);
+            gotoxy(46, 12);
             scanf(" %[^\n]", par.nombre);
-            gotoxy(46, 13);
+            gotoxy(48, 13);
             scanf(" %[^\n]", par.apellido);
             gotoxy(42, 14);
             scanf(" %[^\n]", par.edad);
@@ -368,8 +302,14 @@ void start()
             scanf(" %[^\n]", par.contacto);
             gotoxy(64, 16);
             scanf(" %[^\n]", par.departamento);
+            gotoxy(49, 18);
+            scanf(" %[^\n]", par.car.facultad);
+            gotoxy(43, 19);
+            scanf(" %[^\n]", par.car.nombre);
+            gotoxy(62, 20);
+            cin >> par.car.año;
             updateStudent(par, pos);
-            gotoxy(10,17);
+            gotoxy(10, 22);
             cout << "Registro de inscripcion actualizado... " << endl;
             system("pause");
             break;
@@ -378,7 +318,7 @@ void start()
             system("cls||clear");
             if (UltReg == 0)
             {
-                cout << "No hay registro que eliminar..." << endl;
+                cout << "No hay registros que eliminar..." << endl;
             }
             cout << "Escribe el Id del participante: ";
             cin >> cod;
@@ -416,6 +356,7 @@ void start()
 
         case 6:
             break;
+
         default:
             system("cls||clear");
             cout << "Opcion invalida" << endl;
